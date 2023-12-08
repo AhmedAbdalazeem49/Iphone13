cac3557b6672e3db2262a78e21b630ad1bc16cef
AhmedAbdalazeem49 <99069962+AhmedAbdalazeem49@users.noreply.github.com>
2023-12-08 19:34:15 +0200

    CSS Code
    Start by creating the structure for the design in the index.html file.
    then I styled the structure using CSS code in a style.css file by using colors for text and backgrounds using display flex to display elements.
    then I create two functions to change the image and the body background color when user choose another image form the bottom slider.

---
+++ index.js
@@ -0,0 +1,10 @@
+let img = document.getElementById("image");
+let container = document.querySelector(".container");
+
+function phones(imgSrc) {
+  img.src = imgSrc;
+}
+
+function colors(color) {
+  container.style.background = color;
+}
